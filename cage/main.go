374860ba62f1e4d9cab846b899294986191b5cb5
Tanner Burns <tjburns102@gmail.com>
2019-01-24 18:07:08 -0600

    added certs and are running on https

--- cage/main.go
+++ cage/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"os"
@@ -16,7 +17,7 @@ func main() {
 	logs.InitLogging("House - API", os.Stdout, os.Stdout, os.Stdout, os.Stderr, os.Stderr, os.Stdout)
 
 	//host := "localhost"
-	port := "5000"
+	//port := "5000"
 
 	router, conlogger := routes.NewRouter() // create routes
 
@@ -31,6 +32,25 @@ func main() {
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
 
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+
+	srv := &http.Server{
+		Addr:         ":443",
+		Handler:      handlers.CORS(allowedOrigins, allowedMethods)(router),
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+
 	// Launch server with CORS validations
-	logs.Fatal.Println(http.ListenAndServe(":"+port, handlers.CORS(allowedOrigins, allowedMethods)(router)))
+	logs.Fatal.Println(srv.ListenAndServeTLS("certs/server.rsa.crt", "certs/server.rsa.key"))
 }
